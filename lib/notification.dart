8387b510042928a951743e80b8ece63d52b59c91
Shubhamshaw <shubham.shaw91@gmail.com>
2020-09-01 11:58:12 +0530

    Create notification.dart

---
+++ lib/notification.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/material.dart';
+
+class Notific extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage("assets/kings.jpg"),
+            ),
+            title: Text(
+                "King United Official upload:Tera Baap Aya | Natarang | Tanya Bhusan Choreography | The kings"),
+            trailing: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage("assets/dance.jpeg")),
+                  ),
+            ),
+          ),
+        ),
+        
+      ],
+    );
+  }
+}
